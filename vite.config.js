624a2bcbe96e95bf2374bf7294f70f64339ec118
ffnoobgamer1001-boop <ffnoobgamer1001@gmail.com>
2026-03-10 21:56:16 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+```javascript
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+plugins: [react()],
+// IMPORTANT: Replace 'flexsora-web' with your exact GitHub repository name!
+base: '/
+flexsora/',
+})
+```
